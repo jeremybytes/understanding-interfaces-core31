c570267d1f623b41759c0531412c3ec8ada15fea
Jeremy Clark <jeremy@jeremybytes.com>
2020-04-22 12:27:30 -0700

    Added first web project (in progress)

---
+++ web/completed/02-FutureProofing/PeopleViewer/Controllers/PeopleController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common;
+using Microsoft.AspNetCore.Mvc;
+using People.Library;
+
+namespace PeopleViewer.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult ConcreteType()
+        {
+            List<Person> people;
+
+            var reader = new PersonReader();
+            people = reader.GetPeople();
+
+            ViewData["Title"] = "Using Concrete Type";
+            return View("Index", people);
+        }
+
+        public IActionResult AbstractType()
+        {
+            IEnumerable<Person> people;
+
+            var reader = new PersonReader();
+            people = reader.GetPeople();
+
+            ViewData["Title"] = "Using Abstract Type";
+            return View("Index", people);
+        }
+    }
+}
