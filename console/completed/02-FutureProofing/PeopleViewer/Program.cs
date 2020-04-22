b0fea7167b6d3ab7ae65327a079e28a2d3574c0b
Jeremy Clark <jeremy@jeremybytes.com>
2020-04-22 09:26:06 -0700

    Added initial console applications

---
+++ console/completed/02-FutureProofing/PeopleViewer/Program.cs
@@ -0,0 +1,76 @@
+﻿using Common;
+using People.Library;
+using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace PeopleViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (ShowMenu()) { }
+        }
+
+        private static void FetchWithConcreteType()
+        {
+            WriteLine();
+            WriteLine("=================");
+
+            List<Person> people;
+
+            var reader = new PersonReader();
+            people = reader.GetPeople();
+            foreach (var person in people)
+                WriteLine(person);
+
+            WriteLine("=================");
+            WriteLine();
+            WriteLine("Press any key to continue...");
+            ReadKey();
+        }
+
+        private static void FetchWithAbstractType()
+        {
+            WriteLine();
+            WriteLine("=================");
+
+            IEnumerable<Person> people;
+
+            var reader = new PersonReader();
+            people = reader.GetPeople();
+            foreach (var person in people)
+                WriteLine(person);
+
+            WriteLine("=================");
+            WriteLine();
+            WriteLine("Press any key to continue...");
+            ReadKey();
+        }
+
+        private static bool ShowMenu()
+        {
+            Clear();
+            WriteLine("Choose an option:");
+            WriteLine("1) Use Concrete Type");
+            WriteLine("2) Use Abstraction");
+            WriteLine("3) Exit");
+            Write("\r\nSelect an option: ");
+
+            switch (ReadKey().Key)
+            {
+                case ConsoleKey.D1:
+                    FetchWithConcreteType();
+                    return true;
+                case ConsoleKey.D2:
+                    FetchWithAbstractType();
+                    return true;
+                case ConsoleKey.D3:
+                    return false;
+                default:
+                    return true;
+            }
+        }
+    }
+}
---
+++ console/completed/03-Extensibility/PeopleViewer/Program.cs
@@ -0,0 +1,84 @@
+﻿using PersonReader.Interface;
+using System;
+
+using static System.Console;
+
+namespace PeopleViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (ShowMenu()) { }
+        }
+
+        static void UseWebService()
+        {
+            FetchData("Service");
+        }
+
+        static void UseCSVFile()
+        {
+            FetchData("CSV");
+        }
+
+        static void UseSQLDatabase()
+        {
+            FetchData("SQL");
+        }
+
+        static void FetchData(string readerType)
+        {
+            WriteLine();
+            WriteLine("=================");
+
+            IPersonReader reader = ReaderFactory.GetReader(readerType);
+
+            var people = reader.GetPeople();
+            foreach (var person in people)
+                WriteLine(person);
+
+            WriteLine("=================");
+
+            ShowReaderType(reader);
+
+            WriteLine("=================");
+            WriteLine();
+            WriteLine("Press any key to continue...");
+            ReadKey();
+        }
+
+        private static void ShowReaderType(IPersonReader reader)
+        {
+            WriteLine($"Reader Type: {reader.GetType()}");
+        }
+
+        private static bool ShowMenu()
+        {
+            Clear();
+            WriteLine("Choose an option:");
+            WriteLine("1) Use Web Service");
+            WriteLine("2) Use CSV Text File");
+            WriteLine("3) Use SQL Database");
+            WriteLine("4) Exit");
+            Write("\r\nSelect an option: ");
+
+            switch (ReadKey().Key)
+            {
+                case ConsoleKey.D1:
+                    UseWebService();
+                    return true;
+                case ConsoleKey.D2:
+                    UseCSVFile();
+                    return true;
+                case ConsoleKey.D3:
+                    UseSQLDatabase();
+                    return true;
+                case ConsoleKey.D4:
+                    return false;
+                default:
+                    return true;
+            }
+        }
+    }
+}
